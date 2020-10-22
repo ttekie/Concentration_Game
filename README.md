72fba27e0d71c87109f8555fe0a34e7da9b88194
tesfalem tekie <teshanna72016@gmail.com>
2020-10-22 11:18:07 -0500

    updated file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Concentration_Game
+# Concentration_Game:
+
+This is a simple game created completely with HTML, CSS, and vanilla JavaScript.
+
+ * Selecting DOM elements with JS
+    - Selecting elements by id name
+    - Selecting elements by class name
+    - Selecting child elements of another element
+    - Adding and remove class names from DOM elements
+    - Adding click events to an element
+    - Adding an event listener and listening for events of a type
+ * Working with Arrays
+    - Getting the length of an array
+    - Loop through all elements in an array with for and forEach loop
+    - Using slice() to copy an array
+    - Using splice() to remove items from an array
+ * Working with random numbers
+ * Creating classes with JavaScript
+    - Creating class objects using the function syntax
+    - Adding methods to a class object with the prototype chain
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-  var resetButton = document.getElementById("reset-button");
-  var colors = [];
-  for (var i = 0; i < 10; i++) {
+  let resetButton = document.getElementById("reset-button");
+  let colors = [];
+  for (let i = 0; i < 10; i++) {
     colors.push('square-' + i);
-  }
+  }  // end for loop
   // game square will listen for click events
   function GameSquare(el, color) {
     this.el = el;
@@ -12,7 +12,7 @@ $(document).ready(function() {
     this.isLocked = false;
     this.el.addEventListener("click", this, false);
     this.setColor(color);
-  }
+  }  // end function GameSquare
   // event handler
   GameSquare.prototype.handleEvent = function(e) {
     switch (e.type) {
@@ -22,6 +22,7 @@ $(document).ready(function() {
         }
         this.isOpen = true;
         this.el.classList.add('flip');
+        checkGame(this);   // check the game here
     }
   }
   GameSquare.prototype.reset = function() {
@@ -38,5 +39,78 @@ $(document).ready(function() {
     this.color = color;
     this.el.children[0].children[1].classList.add(color);
   }
+
+  let gameSquares = [];
+    // setup the game
+function setupGame() {
+  let array = document.getElementsByClassName("game-square");
+    // Get the array of 8 random color pairs
+  let randomColors = getSomeColors();
+  for (let i = 0; i < array.length; i++) {
+      // Get a random index
+    let index = random(randomColors.length);
+      // Get the color at that index
+    let color = randomColors.splice(index, 1)[0];
+      // Use that color to initialize the GameSquare
+    gameSquares.push(new GameSquare(array[i], colors[0]));
+  }
+}   // end function setupGame
+
+function random(n) {
+  return Math.floor(Math.random() * n);
+}  // end function random
+
+function getSomeColors() {
+  let colorscopy = colors.slice();
+  let randomColors = [];
+  for (let i = 0; i < 8; i++) {
+    let index = random(colorscopy.length);
+    randomColors.push(colorscopy.splice(index, 1)[0]);
+  }
+  return randomColors.concat(randomColors.slice());
+}  // end function getSomeColors
+
+let firstSquare = null;
+
+function checkGame(gameSquare) {
+  if (firstSquare === null) {
+    firstSquare = gameSquare;
+    return;
+  }
+
+  if (firstSquare.color === gameSquare.color) {
+    firstSquare.lock();
+    gameSquare.lock();
+  } else {
+    let a = firstSquare;
+    let b = gameSquare;
+    setTimeout(function() {
+      a.reset();
+      b.reset();
+      firstSquare = null;
+    }, 400);
+  }
+  firstSquare = null;
+}  // end function checkGame
+
+function randomizeColors() {
+  let randomColors = getSomeColors();
+  gameSquares.forEach(function(gameSquare) {
+    let color = randomColors.splice(random(randomColors.length), 1)[0];
+    gameSquare.setColor(color);
+  });
+}  // end function randomizeColors
+
+function clearGame() {
+  gameSquares.forEach(function(gameSquare) {
+    gameSquare.reset();
+  });
+  setTimeout(function() {
+    randomizeColors();
+  }, 500);
+}  // end function clearGame
+
+setupGame();  //invoke setupGame
+clearGame();  // invoke clearGame
 
 });
